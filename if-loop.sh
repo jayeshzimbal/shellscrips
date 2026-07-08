253dc994480b14531f7f245a89343bc31e03ab28
Jayesh <jayeshzimbal@gmail.com>
2026-07-08 13:15:49 +0530

    loops in shell

---
+++ if-loop.sh
@@ -0,0 +1,13 @@
+
+#write the shellscript to print numbers divided by 3 and 5 and not 15
+
+#!/bin/bash
+
+for i in {1..100}; do 
+
+if (['expr $i % 3' == 0 ] || ['expr $i % 5' == 0 ]) && ['expr $i % 15' != 0 ];
+
+then 
+    echo $i
+fi;
+done
